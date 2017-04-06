1ed2df9702018a47bd526dbbc446c77e6dea2f88
swapnasheela <swapnasheela.study@gmail.com>
2017-04-06 16:17:36 +0530

    modified file name calculator

---
+++ lecture02/app.js
@@ -0,0 +1,23 @@
+var student = {
+  name:"",
+  type:"student"
+};
+document.addEventListener('DOMContentLoaded', contentLoaded);
+
+function contentLoaded(event){
+  document.getElementById('name').addEventListener("keyup",keyup);
+}
+function keyup(event){
+  calculateNumericOutput();
+}
+function calculateNumericOutput(){
+  student.name = document.getElementById('name').value;
+  var totalNameValue = 0;
+
+    for(var i=0; i<student.name.length;i++){
+      totalNameValue += student.name.charCodeAt(i);
+    }
+     //insert result into page
+     var output = "Total numeric value of person's name is " + totalNameValue;
+     document.getElementById('output').innerText = output;
+}
